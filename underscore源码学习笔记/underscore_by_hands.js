63914ea47a8d8664f368291e2acf4dd914918678
ginnko <yybnbn@126.com>
2018-01-29 22:51:03 +0800

    throttle in progress

--- underscore源码学习笔记/underscore_by_hands.js
+++ underscore源码学习笔记/underscore_by_hands.js
@@ -998,7 +998,37 @@ _.throttle = function(func, wait, options){
     opstions = {};
   }
   var later = function(){
-    previous = 
+    previous = opstions.leading === false ? 0: _.now();
+    timeout = null;
+    result = func.apply(context, args);
+    if(!timeout){
+      context = args = null;
+    }
+  };
+  return function(){
+    var now = _.now();
+    if(!previous && options.leading === false){
+      previous = now;
+    }
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+
+    if(remaining <= 0 || remaining > wait){
+      if(timeout){
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+
+      if(!timeout){
+        result = func.apply(context, args);
+      }
+    }else if(!timeout && options.trailing !== false){
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
   };
 };
 
