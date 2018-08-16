839e84cd45b3ed80fa41b10867524c3e7b4c90c7
yangyin <yybnbn@126.com>
2018-08-16 18:10:21 +0800

    going to finish closure

---
+++ javascript设计模式与开发实践/test.js
@@ -0,0 +1,21 @@
+var mult = (function() {
+  var cache = {};
+  var calculate = function() {
+    var a = 1;
+    console.log(arguments.length);
+    for (var i = 0, l = arguments.length; i < l; i++) {
+      a = a * arguments[i];
+    }
+    return a;
+  };
+
+  return function() {
+    var args = Array.prototype.join.call(arguments, ',');
+    if (args in cache) {
+      return cache[args];
+    }
+    return cache[args] = calculate.apply(null, arguments);
+  }
+})();
+
+mult(1, 2, 3);
